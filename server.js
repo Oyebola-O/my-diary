c778b5ffa9bb1897cf1dfc1bfe133164826eb8af
Oyebola <oyebolaodukoya@gmail.com>
2018-07-24 23:49:44 +0100

    Write test and implement Get entries Update readme

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-// Import statements
 import express from 'express';
 import bodyParser from 'body-parser';
+import router from './routes/api_endpoints';
 
-// Define Constants
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Use defined variables
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(require('./routes/api_endpoints'));
+app.use(bodyParser.json());
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server us listening on port: ${port}`);
 });
 
-export {app};
+
+export default app;
