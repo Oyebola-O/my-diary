c503bfd64694d71cb6aad991a4b9ae0d6e73f59e
Oyebola <oyebolaodukoya@gmail.com>
2018-07-25 02:04:03 +0100

    Write PUT test to modify entry and write it's api Handle corner cases

--- routes/api_endpoints.js
+++ routes/api_endpoints.js
@@ -36,15 +36,31 @@ router.get('/api/v1/entries/:id?', (req, res) => {
   if (!id) {
     res.send(entries);
   } else {
-    // TODO: Case 2, when there is an id
-    if(id >= 0 && id < entries.length){
+    // Corner case
+    if (id >= 0 && id < entries.length) {
       res.send(entries[id]);
     } else {
       res.status(404);
-      res.send({message: "Error, there's no entry with that id"});
+      res.send({ message: "Error, there's no entry with that id" });
     }
   }
 });
 
 
+// Put entries
+router.put('/api/v1/entries/:id', (req, res) => {
+  const id = req.params.id;
+  if (id >= 0 && id < entries.length) {
+    const title = req.body.title;
+    const text = req.body.text;
+
+    entries[id].title = title;
+    entries[id].text = text;
+
+    res.send(entries[id]);
+  } else {
+    res.status(404);
+    res.send({ message: "Error, there's no entry with that id" });
+  }
+});
 export default router;
