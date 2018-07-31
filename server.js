24bd2eda30411519e49691168ff66d216522d21d
Oyebola <oyebolaodukoya@gmail.com>
2018-07-31 15:25:26 +0100

    Rewrite functions using middlewares

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mustacheExpress from 'mustache-express';
 import bodyParser from 'body-parser';
 import router from './routes/api_endpoints';
+import auth from './routes/auth'
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -14,7 +15,8 @@ app.set('view engine', 'mustache');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get('/');
-app.use('', router);
+app.use('/api/v1', router);
+app.use('/api/v1', auth);
 
 app.listen(port, () => {
   console.log(`Server us listening on port: ${port}`);
